c3cff945f8c5f3e38a34de448791b4a1781eb9b9
gbonwick-gh <georgebonwick@hotmail.com>
2026-08-18 15:47:49 +0100

    Refactor logo paths in stations.js
    Updated logo paths for radio stations to remove the 'logos/' prefix.

--- stations.js
+++ stations.js
@@ -2,239 +2,240 @@ const stations = [
     {
         name: "Absolute Radio",
         strapline: "Where real music matters.",
-        logo: "logos/absoluteradio.png",
+        logo: "absoluteradio.png",
         id: "absoluteradio"
     },
     {
         name: "Absolute Radio 80s",
         strapline: "",
-        logo: "logos/absoluteradio80s.png",
+        logo: "absoluteradio80s.png",
         id: "absoluteradio80s"
     },
     {
         name: "Absolute Radio 90s",
         strapline: "",
-        logo: "logos/absoluteradio90s.png",
+        logo: "absoluteradio90s.png",
         id: "absoluteradio90s"
     },
 
     {
         name: "BBC Radio 1",
         strapline: "",
-        logo: "logos/bbcradio1.png",
+        logo: "bbcradio1.png",
         id: "bbcradio1"
     },
     {
         name: "BBC Radio 1 Anthems",
         strapline: "",
-        logo: "logos/bbcradio1anthems.png",
+        logo: "bbcradio1anthems.png",
         id: "bbcradio1anthems"
     },
     {
         name: "BBC Radio 1 Dance",
         strapline: "",
-        logo: "logos/bbcradio1dance.png",
+        logo: "bbcradio1dance.png",
         id: "bbcradio1dance"
     },
     {
         name: "BBC Radio 1Xtra",
         strapline: "",
-        logo: "logos/bbcradio1xtra.png",
+        logo: "bbcradio1xtra.png",
         id: "bbcradio1xtra"
     },
     {
         name: "BBC Radio 2",
         strapline: "",
-        logo: "logos/bbcradio2.png",
+        logo: "bbcradio2.png",
         id: "bbcradio2"
     },
     {
         name: "BBC Radio 5 Live",
         strapline: "",
-        logo: "logos/bbcradio5live.png",
+        logo: "bbcradio5live.png",
         id: "bbcradio5live"
     },
     {
         name: "BBC Radio 6 Music",
         strapline: "",
-        logo: "logos/bbcradio6music.png",
+        logo: "bbcradio6music.png",
         id: "bbcradio6music"
     },
+
     {
         name: "Capital",
         strapline: "The UK's No.1 Hit Music Station",
-        logo: "logos/capital.png",
+        logo: "capital.png",
         id: "capital"
     },
     {
         name: "Capital Anthems",
         strapline: "The songs Capital made famous",
-        logo: "logos/capitalanthems.png",
+        logo: "capitalanthems.png",
         id: "capitalanthems"
     },
     {
         name: "Capital Dance",
         strapline: "The UK's Official Dance Station",
-        logo: "logos/capitaldance.png",
+        logo: "capitaldance.png",
         id: "capitaldance"
     },
     {
         name: "Capital XTRA",
         strapline: "The freshest hip-hop, grime, garage, RnB and afrobeats",
-        logo: "logos/capitalxtra.png",
+        logo: "capitalxtra.png",
         id: "capitalxtra"
     },
 
     {
         name: "Classic",
         strapline: "",
-        logo: "logos/classic.png",
+        logo: "classic.png",
         id: "classic"
     },
 
     {
         name: "Gold",
         strapline: "",
-        logo: "logos/gold.png",
+        logo: "gold.png",
         id: "gold"
     },
 
     {
         name: "Greatest Hits Radio",
         strapline: "Good Times Sound Like This",
-        logo: "logos/greatesthitsradio.png",
+        logo: "greatesthitsradio.png",
         id: "greatesthitsradio"
     },
     {
         name: "Greatest Hits Radio 60s",
         strapline: "",
-        logo: "logos/greatesthitsradio60s.png",
+        logo: "greatesthitsradio60s.png",
         id: "greatesthitsradio60s"
     },
     {
         name: "Greatest Hits Radio 70s",
         strapline: "",
-        logo: "logos/greatesthitsradio70s.png",
+        logo: "greatesthitsradio70s.png",
         id: "greatesthitsradio70s"
     },
     {
         name: "Greatest Hits Radio 80s",
         strapline: "",
-        logo: "logos/greatesthitsradio80s.png",
+        logo: "greatesthitsradio80s.png",
         id: "greatesthitsradio80s"
     },
 
     {
         name: "Heart",
         strapline: "Turn up the feel good!",
-        logo: "logos/heart.png",
+        logo: "heart.png",
         id: "heart"
     },
     {
         name: "Heart 70s",
         strapline: "Non-stop 70s feel good!",
-        logo: "logos/heart70s.png",
+        logo: "heart70s.png",
         id: "heart70s"
     },
     {
         name: "Heart 80s",
         strapline: "Non-stop 80s feel good!",
-        logo: "logos/heart80s.png",
+        logo: "heart80s.png",
         id: "heart80s"
     },
     {
         name: "Heart 90s",
         strapline: "Non-stop 90s feel good!",
-        logo: "logos/heart90s.png",
+        logo: "heart90s.png",
         id: "heart90s"
     },
     {
         name: "Heart 00s",
         strapline: "Non-stop 00s feel good!",
-        logo: "logos/heart00s.png",
+        logo: "heart00s.png",
         id: "heart00s"
     },
     {
         name: "Heart 10s",
         strapline: "Non-stop 10s feel good!",
-        logo: "logos/heart10s.png",
+        logo: "heart10s.png",
         id: "heart10s"
     },
     {
         name: "Heart Dance",
         strapline: "Non-stop Club Classics!",
-        logo: "logos/heartdance.png",
+        logo: "heartdance.png",
         id: "heartdance"
     },
 
     {
         name: "Hits Radio",
         strapline: "",
-        logo: "logos/hitsradio.png",
+        logo: "hitsradio.png",
         id: "hitsradio"
     },
 
     {
         name: "KISS",
         strapline: "",
-        logo: "logos/kiss.png",
+        logo: "kiss.png",
         id: "kiss"
     },
     {
         name: "KISSTORY",
         strapline: "",
-        logo: "logos/kisstory.png",
+        logo: "kisstory.png",
         id: "kisstory"
     },
 
     {
         name: "Magic Radio",
         strapline: "The Best Variety from the 80s to Now",
-        logo: "logos/magicradio.png",
+        logo: "magicradio.png",
         id: "magicradio"
     },
 
     {
         name: "Radio X",
         strapline: "Get into the music",
-        logo: "logos/radiox.png",
+        logo: "radiox.png",
         id: "radiox"
     },
 
     {
         name: "Smooth",
         strapline: "Always the best music",
-        logo: "logos/smooth.png",
+        logo: "smooth.png",
         id: "smooth"
     },
     {
         name: "Smooth 70s",
         strapline: "Always the best 70s music mix",
-        logo: "logos/smooth70s.png",
+        logo: "smooth70s.png",
         id: "smooth70s"
     },
     {
         name: "Smooth 80s",
         strapline: "Always the best 80s music mix",
-        logo: "logos/smooth80s.png",
+        logo: "smooth80s.png",
         id: "smooth80s"
     },
     {
         name: "Smooth Chill",
         strapline: "Music to chill to",
-        logo: "logos/smoothchill.png",
+        logo: "smoothchill.png",
         id: "smoothchill"
     },
     {
         name: "Smooth Country",
         strapline: "Just great country",
-        logo: "logos/smoothcountry.png",
+        logo: "smoothcountry.png",
         id: "smoothcountry"
     },
     {
         name: "Smooth Soul",
         strapline: "The UK's soul station",
-        logo: "logos/smoothsoul.png",
+        logo: "smoothsoul.png",
         id: "smoothsoul"
     }
 ];
